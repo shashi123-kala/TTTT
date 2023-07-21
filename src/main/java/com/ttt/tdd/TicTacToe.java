72d033e1c2e2ee2fa2edd1f34b2ec499bf5fd255
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-21 17:26:06 +0530

    Add: Check if its occupied

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -2,7 +2,13 @@ package com.ttt.tdd;
 
 
 public class TicTacToe {
-	
+
 	private Character[][] board = { { '\0', '\0', '\0' }, { '\0', '\0', '\0' }, { '\0', '\0', '\0' } };
+
+	public void play(int row, int column) {
+		if (board[column - 1][row - 1] != '\0') {
+            throw new RuntimeException("Field is occupied!");
+        } 
+	}
 
 }
